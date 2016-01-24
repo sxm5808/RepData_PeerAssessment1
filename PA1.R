c327ec29bc35b0840c542f3cd162d193d919145a
sxm5808 <sanjay.mallampati@gmail.com>
2016-01-24 13:36:01 -0600

    Submit_Assignment_for P1
    Submit_Assignment_for P1 by sxm5808

--- PA1.R
+++ PA1.R
@@ -1,37 +1,16 @@
----
-title: "PA1_template"
-author: "sxm5808"
-date: "January 23, 2016"
-output: html_document
----
-
-## **Coursera Course : Reproducable Research- Project 1.**
-
-### Loading and preprocessing the data
-
-```{r, echo=TRUE}
 unzip(zipfile="activity.zip")
 rawdata <- read.csv("activity.csv")
-```
-
-### What is mean total number of steps taken per day?
-
-```{r, echo=TRUE}
-#Aggregate for daily totals
 attach(rawdata)
 daily<- aggregate(steps,by=list(date),FUN=sum)
+#daily<-aggregate(rawdata$steps, list(rawdata$date), sum)
 detach(rawdata)
 names(daily)<-c("day","steps")
-#Plot a Histogram
+#totalsteps <- data.frame(tapply(rawdata$steps, rawdata$date, FUN=sum, na.rm=TRUE))
+#hist(daily$steps,na.rm=TRUE,breaks=61)
 library(ggplot2)
 qplot(daily$steps, binwidth = 1000, xlab = "Total Steps/day")
-#Calculate Mean and Medians
 mean(daily$steps,na.rm = TRUE)
 median(daily$steps,na.rm = TRUE)
-```
-
-### What is the average daily activity pattern?
-```{r, echo=TRUE}
 #Average across days per interval
 attach(rawdata)
 intervals<-aggregate(steps,by=list(interval),mean,na.rm=TRUE)
@@ -40,17 +19,9 @@ names(intervals)<-c("Interval","Avgsteps")
 #Plotting a line chart
 ggplot(data = intervals, aes(x = Interval, y = Avgsteps)) + geom_line() + xlab("5-minute interval") + 
   ylab("Avg. # of steps taken")
-# Which Internval has maximum Value 
 intervals[which.max(intervals$Avgsteps), ]
-
-
-```
-### Imputing missing values
-```{r, echo=TRUE}
-# Missing Values
 missing <- sum(is.na(rawdata$steps))
 missing
-#Replacing NA values from the Intervals data frame(which contains Interval means) .
 impute <- function(steps, interval) {
   filled <- NA
   if (!is.na(steps)) 
@@ -60,20 +31,13 @@ impute <- function(steps, interval) {
 names(intervals)<-c("interval","steps")
 rawdata_filled<-rawdata
 rawdata_filled$steps<-mapply(impute,rawdata_filled$steps,rawdata_filled$interval)
-#Making a Histogram on the new data frame
+
 attach(rawdata_filled)
 daily_filled<- aggregate(steps,by=list(date),FUN=sum)
 detach(rawdata_filled)
 names(daily_filled)<-c("day","steps")
 qplot(daily_filled$steps, binwidth = 1000, xlab = "Total Steps/day")
 
-#Calculate Mean and Medians
-mean(daily_filled$steps,na.rm = TRUE)
-median(daily_filled$steps,na.rm = TRUE)
-
-```
-### Are there differences in activity patterns between weekdays and weekends?
-```{r, echo=TRUE}
 #Determine Weekend or Weekday
 wday<- function(date)
   {
@@ -86,10 +50,15 @@ else
 
 rawdata_filled$date <-as.Date(rawdata_filled$date)
 rawdata_filled$day<- sapply(rawdata_filled$date,FUN = wday)
-
-#Plotting Intervals between Weekend and weekday.More rest on weekend :)
+#Plotting Intervals between Weekend and weekday.More rest on weekend:)
 averages <- aggregate(steps ~ interval + day, data = rawdata_filled, mean)
 ggplot(averages, aes(interval, steps)) + geom_line() + facet_grid(day ~ .) + 
   xlab("5-minute interval") + ylab("Number of steps")
 
-```
+##KNITTING HTML
+
+library(knitr)
+rmarkdown::render("PA1_template.Rmd")
+
+
+
